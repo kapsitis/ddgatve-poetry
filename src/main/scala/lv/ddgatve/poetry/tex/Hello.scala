50ec681f87a1590b17e00a72df68415832b2f163
Kalvis Apsitis <kalvis.apsitis@gmail.com>
2019-03-26 20:08:39 +0200

    Sample JSON

--- src/main/scala/lv/ddgatve/poetry/tex/Hello.scala
+++ src/main/scala/lv/ddgatve/poetry/tex/Hello.scala
@@ -1,8 +1,16 @@
 package lv.ddgatve.poetry.tex
 
+import scala.io.Source
+import play.api.libs.json._
+ 
+
 object Hello {
   def main(args:Array[String]): Unit = {
-    print("Sveicinaati!")
+    val rawJson = Source.fromFile(
+        "src/main/resources/talu-celu-vejs.json").getLines.mkString
+    val json1 = Json.parse(rawJson)
+    val title = (json1 \ "book" \ "title").as[String]
+    print("title is " + title)
   }
   
 }
